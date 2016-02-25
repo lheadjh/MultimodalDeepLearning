778e2ae28f1540722369c5c4add8bc65a9634a3b
jhlee <lheadjh@naver.com>
2016-02-25 22:52:37 +0900

    fix sess

--- ver_tensorflow/DNNIMAGE.py
+++ ver_tensorflow/DNNIMAGE.py
@@ -82,7 +82,7 @@ with tf.device('/gpu:' + GPUNUM):
     init = tf.initialize_all_variables()
 
     # Launch the graphe
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
         sess.run(init)
         #Training cycle
         for epoch in range(training_epochs):
