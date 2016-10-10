636bfa484b2ebebcae91d60c4c3f0448f3e27309
jhlee <lheadjh@naver.com>
2016-10-10 18:10:15 +0900

    add video data load

--- ver_tensorflow/mdl_data.py
+++ ver_tensorflow/mdl_data.py
@@ -18,6 +18,7 @@ class YLIMED():
         self.VID = []
         self.LABEL = []
         self.SET = []
+        self.TIME = []
         self._epochs_completed = 0
         self._index_in_epoch = 0
         self.__summary_data()
@@ -26,8 +27,11 @@ class YLIMED():
         self._Y = []
         if not (os.path.isfile('YLIMED_info.tmp')):
             self.__initial_data_info()
+        else:
+            self.__resummary_data()
 
     def __summary_data(self):
+        # read from csv file and save VID, LABEL, SET(tr or te) data
         f = open(self.pathInfo, 'rb')
         inforeader = csv.reader(f)
         next(inforeader)
@@ -49,7 +53,8 @@ class YLIMED():
                 trainingset[EVENT[info[7]]] += 1
             else:
                 testset[EVENT[info[7]]] += 1
-
+        f.close()
+            
         print '-------------------------------------SUMMARY-------------------------------------'
         print 'Total            =', int(sum(labelset))
         print 'Tr / Te ratio    =', trteratio
@@ -57,9 +62,51 @@ class YLIMED():
         print 'Training set     =', trainingset
         print 'Test set         =', testset
         print '---------------------------------------------------------------------------------'
+        
+    def __resummary_data(self):
+        # read from tmp file
+        del(self.VID)
+        del(self.LABEL)
+        del(self.SET)
+        self.VID = []
+        self.LABEL = []
+        self.SET = []
 
-        f.close()
+        tmpinfo = open('YLIMED_info.tmp', 'r')
+        tmpdata = tmpinfo.readlines()
 
+        trteratio = np.zeros(2)
+        labelset = np.zeros(10)
+        trainingset = np.zeros(10)
+        testset = np.zeros(10)
+
+        for tmp in tmpdata:
+            tmp = tmp.split()
+            if tmp[1] == 'MISS':
+                continue
+            
+            self.VID.append(tmp[0])
+            self.TIME.append(tmp[1])
+            self.LABEL.append(tmp[2])
+            self.SET.append(tmp[3])
+            
+            labelset[EVENT[tmp[2]]] += 1
+            trteratio[RATIO[tmp[3]]] += 1
+
+            if tmp[3] == 'Training':
+                trainingset[EVENT[tmp[2]]] += 1
+            else:
+                testset[EVENT[tmp[2]]] += 1
+        tmpinfo.close()
+            
+        print '-------------------------------------RESUMMARY-------------------------------------'
+        print 'Total            =', int(sum(labelset))
+        print 'Tr / Te ratio    =', trteratio
+        print 'Label set        =', labelset
+        print 'Training set     =', trainingset
+        print 'Test set         =', testset
+        print '---------------------------------------------------------------------------------'
+        
     def __initial_data_info(self):
         print 'Initial data info...'
         f = open('YLIMED_info.tmp', 'w')
@@ -68,19 +115,22 @@ class YLIMED():
         total = len(self.VID)
         count = 0
 
-        for tVID in self.VID:
+        for i in range(0, len(self.VID)):
             #check both file exist
-            temp_aud_file = self.pathAud + '/' + tVID + '.mfcc20.ascii'
-            temp_img_file = self.pathImg + '/' + tVID + '.fc7.txt'
-            if not (os.path.isfile(temp_aud_file) and os.path.isfile(temp_img_file)):
-                total -= 1
-                continue
+            temp_aud_file = self.pathAud + '/' + self.VID[i] + '.mfcc20.ascii'
+            temp_img_file = self.pathImg + '/' + self.VID[i] + '.fc7.txt'
+
             #set progress bar
             count += 1
             progress = int(float(count) / float(total) * float(100))
             sys.stdout.write('\r')
             sys.stdout.write("[%-20s] %d%% %d sec" % ('='*(progress/5), progress, time.time() - starttime))
             sys.stdout.flush()
+            
+            #if exist missing file
+            if not (os.path.isfile(temp_aud_file) and os.path.isfile(temp_img_file)):
+                f.write(self.VID[i] + ' ' + 'MISS' + ' ' + self.LABEL[i] + ' ' + self.SET[i] + '\n')
+                continue
             #open file
             audFile = open(temp_aud_file, 'r')
             audData = audFile.readlines()
@@ -93,11 +143,12 @@ class YLIMED():
             else:
                 range_len=len(imgData)
 
-            f.write(tVID + ' ' + str(range_len) + '\n')
+            f.write(self.VID[i] + ' ' + str(range_len) + ' ' + self.LABEL[i] + ' ' + self.SET[i] + '\n')
             imgFile.close()
 
         f.close()
-
+        
+    # -- get frame data for global randomly
     def __get_part_data(self, Aud_Img_Lab, tr_or_te):
         tmpinfo = open('YLIMED_info.tmp', 'r')
         tmpdata = tmpinfo.readlines()
@@ -115,16 +166,12 @@ class YLIMED():
             line = line.split()
             tVID = line[0]
             range_len = line[1]
+            if range_len == 'MISS':
+                continue
             set = self.SET[self.VID.index(tVID)]
 
             if set != tr_or_te:
                 continue
-
-            if tr_or_te == 'Test' and Aud_Img_Lab == 'VID':
-                for i in range(int(range_len)):
-                    output.append(tVID)
-                continue
-
 
             if Aud_Img_Lab == 'Lab':
                 for i in range(int(range_len)):
@@ -156,94 +203,38 @@ class YLIMED():
         print ', finish'
         return output
     
-    def __new_get_part_data(self, Aud_Img_Lab, tr_or_te):
-        tmpinfo = open('YLIMED_info.tmp', 'r')
-        tmpdata = tmpinfo.readlines()
-        starttime = time.time()
-        total = len(tmpdata)
-        count = 0
+    # -- get frame data per vid
+    def get_vid_data(self, line, Aud_Img_Lab):
         output = []
-        for line in tmpdata:
-            count += 1
-            progress = int(float(count) / float(total) * float(100))
-            sys.stdout.write('\r')
-            sys.stdout.write("[%-20s] %d%% %d sec" % ('='*(progress/5), progress, time.time() - starttime))
-            sys.stdout.flush()
+        line = line.split()
+        tVID = line[0]
+        range_len = line[1]
+        
+        if Aud_Img_Lab == 'Aud':
+            temp_file = self.pathAud + '/' + tVID + '.mfcc20.ascii'
+            f = open(temp_file, 'r')
+            data = f.readlines()
+            f.close()
+        elif Aud_Img_Lab == 'Img':
+            temp_file = self.pathImg + '/' + tVID + '.fc7.txt'
+            f = open(temp_file, 'r')
+            data = f.readlines()
+            f.close()
+        else:
+            print "Error in get_vid_data"
+            raise SystemExit
 
-            line = line.split()
-            tVID = line[0]
-            range_len = line[1]
-            set = self.SET[self.VID.index(tVID)]
-
-            if set != tr_or_te:
-                continue
-
-            if tr_or_te == 'Test' and Aud_Img_Lab == 'VID':
-                for i in range(40):
-                    output.append(tVID)
-                continue
-
-            if Aud_Img_Lab == 'Lab':
-                for i in range(40):
-                    label = EVENT[self.LABEL[self.VID.index(tVID)]]
-                    output.append(label)
-            else:
-                if Aud_Img_Lab == 'Aud':
-                    temp_file = self.pathAud + '/' + tVID + '.mfcc20.ascii'
-                    f = open(temp_file, 'r')
-                    data = f.readlines()
-                    f.close()
-                elif Aud_Img_Lab == 'Img':
-                    temp_file = self.pathImg + '/' + tVID + '.fc7.txt'
-                    f = open(temp_file, 'r')
-                    data = f.readlines()
-                    f.close()
-                    
-                
-                if int(range_len)-1 > 40:
-                    if Aud_Img_Lab == 'Aud':
-                        for i in range(40):
-                            add = []
-                            for j in range(100):
-                                add += [float(x) for x in data[i*100+j].split()]
-                            output.append(add)
-                    elif Aud_Img_Lab == 'Img':
-                        for i in range(40):
-                            add = []
-                            add = [float(x) for x in data[i].split()]
-                            output.append(add)
-                else:
-                    if Aud_Img_Lab == 'Aud':
-                        checkout = 0
-                        while(1):
-                            for i in range(int(range_len)):
-                                add = []
-                                for j in range(100):
-                                    add += [float(x) for x in data[i*100+j].split()]
-                                output.append(add)
-                                checkout += 1
-                                if checkout == 40:
-                                    break
-                            if checkout == 40:
-                                break
-                    elif Aud_Img_Lab == 'Img':
-                        checkout = 0
-                        while(1):
-                            for i in range(int(range_len)):
-                                add = []
-                                add = [float(x) for x in data[i].split()]
-                                output.append(add)
-                                checkout += 1
-                                if checkout == 40:
-                                    break
-                            if checkout == 40:
-                                break
-
-        tmpinfo.close()
+        for i in range(int(range_len)):
+            add = []
+            if Aud_Img_Lab == 'Aud':
+                for j in range(100):
+                    add += [float(x) for x in data[i*100+j].split()]
+            elif Aud_Img_Lab == 'Img':
+                add = [float(x) for x in data[i].split()]
+            output.append(add)
         output = np.asarray(output)
-        print ', finish'
         return output
-    
+        
     #Source reference: https://github.com/aymericdamien/TensorFlow-Examples.git/input_data.py
     def next_batch(self, X, Y, batch_size, total_size):
         """Return the next `batch_size` examples from this data set."""
@@ -295,62 +286,45 @@ class YLIMED():
     def get_y_test(self):
         print 'Load Test Label Data'
         return self.__get_part_data('Lab', 'Test')
-    def get_testVID(self):
-        return self.__get_part_data('VID', 'Test')
 
-    
-    def new_get_aud_X_train(self):
-        print 'Load New Training Audio Data'
-        return self.__new_get_part_data('Aud', 'Training')
-    def new_get_img_X_train(self):
-        print 'Load New Training Image Data'
-        return self.__new_get_part_data('Img', 'Training')
-    def new_get_y_train(self):
-        print 'Load New Training Label Data'
-        return self.__new_get_part_data('Lab', 'Training')
-    
-    
-    def new_get_aud_X_test(self):
-        print 'Load New Test Audio Data'
-        return self.__new_get_part_data('Aud', 'Test')
-    def new_get_img_X_test(self):
-        print 'Load New Test Image Data'
-        return self.__new_get_part_data('Img', 'Test')
-    def new_get_y_test(self):
-        print 'Load New Test Label Data'
-        return self.__new_get_part_data('Lab', 'Test')
-#TODO
-        # print 'Pickling...'
-        #
-        # y_train = np.asarray(y_train)
-        # y_test = np.asarray(y_test)
-        # print 'y_train num:     %d' % len(y_train), y_train.shape
-        # print 'y_test num:      %d' % len(y_test), y_test.shape
-        # f = open('YLDMED_y.pkl', 'wb')
-        # temp = y_train, y_test
-        # cPickle.dump(temp, f)
-        # f.close()
-        #
-        # aud_X_train = np.asarray(aud_X_train)
-        # aud_X_test = np.asarray(aud_X_test)
-        # print 'aud_X_train num: %d' % len(aud_X_train), aud_X_train.shape
-        # print 'aud_X_test num:  %d' % len(aud_X_test), aud_X_test.shape
-        # f = open('YLDMED_aud_X.pkl', 'wb')
-        # temp = aud_X_train, aud_X_test
-        # cPickle.dump(temp, f)
-        # f.close()
-        #
-        # img_X_train = np.asarray(img_X_train)
-        # img_X_test = np.asarray(img_X_test)
-        # print 'img_X_train num: %d' % len(img_X_train), img_X_train.shape
-        # print 'img_X_test num:  %d' % len(img_X_test), img_X_test.shape
-        # f = open('YLDMED_img_X.pkl', 'wb')
-        # temp = img_X_train, img_X_test
-        # cPickle.dump(temp, f)
-        # f.close()
-
+    def get_vid_info(self, tr_or_te):
+        f = open('YLIMED_info.tmp', 'r')
+        lines = f.readlines()
+        out = []
+        for tmp in lines:
+            tmp = tmp.split()
+            if tmp[1] == 'MISS':
+                continue
+            if tmp[3] == tr_or_te:
+                tmp[2] = str(EVENT[tmp[2]])
+                
+                line = tmp[0] + ' ' + tmp[1] + ' ' + tmp[2] + ' ' + tmp[3]
+                out.append(line)
+        out = np.asarray(out)
+        return out
+        
 if __name__ == '__main__':
     data = YLIMED('YLIMED_info.csv', '/DATA/YLIMED150924/audio/mfcc20', '/DATA/YLIMED150924/keyframe/fc7')
-    #temp = data.new_get_aud_X_train()
-    temp = data.new_get_y_train()
-    print temp.shape
+    
+    vtr = data.get_vid_info('Training')
+    vte = data.get_vid_info('Test')
+    print vtr[1]
+    #test = data.get_vid_data(vtr[1], 'aud')
+    
+    #print test.shape
+    
+    '''
+    y_train = data.get_y_train()
+    X_img_train = np.zeros(len(y_train))
+    X_aud_train = np.zeros(len(y_train))
+
+    y_test = data.get_y_test()
+    X_img_test = np.zeros(len(y_test))
+    X_aud_test = np.zeros(len(y_test))
+    
+    total_batch = int(len(y_train)/32)
+    for i in range(total_batch):
+        batch_x_aud, batch_x_img, batch_ys, finish = data.next_batch_multi(X_aud_train, X_img_train, y_train, 32, len(y_train))
+    for i in range(int(len(y_test))):
+        batch_x_aud, batch_x_img, batch_ys, finish = data.next_batch_multi(X_aud_test, X_img_test, y_test, 1, len(y_test))
+    '''
